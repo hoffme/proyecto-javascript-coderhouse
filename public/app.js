fa453d516398c58cc7bb2c53d411e45f0e8fd24a
Juan Coronel <phjocoronel@gmail.com>
2021-02-04 23:31:40 -0300

    agregando objetos materia prima y precio

--- public/app.js
+++ public/app.js
@@ -1,4 +1,38 @@
-const materiasPrimas = []
+let ultimoIndiceMateriaPrima = 0;
+const materiasPrimas = [];
+
+class Precio {
+    constructor(precio, cantidad) {
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    importePorCantidad(cantidad) {
+        return this.precio * cantidad / this.cantidad;
+    }
+}
+
+class MateriaPrima {
+    constructor(nombre, unidad, precio = null) {
+        ultimoIndiceMateriaPrima++;
+
+        this.id = ultimoIndiceMateriaPrima;
+        this.nombre = nombre;
+        this.unidad = unidad;
+
+        this.precio = precio;
+    }
+
+    definirPrecio(precio) {
+        this.precio = precio;
+    }
+
+    importePorCantidad(cantidad) {
+        if (!this.precio) throw new Error("Precio no definido");
+        
+        return this.precio.importePorCantidad(cantidad);
+    }
+}
 
 const ingresarMateriaPrima = () => {
     const nombre = prompt("nombre")
@@ -7,27 +41,37 @@ const ingresarMateriaPrima = () => {
         return;
     }
 
-    const cantidad = parseFloat(prompt("cantidad"))
-    if (isNaN(cantidad) || cantidad <= 0) {
-        alert("Cantidad negativa o cero invalida");
-        return;
-    }    
-    
-    const medida = prompt("medida")
-    if (medida.length === 0) {
-        alert("Medida muy corta");
+    const unidad = prompt("unidad de medida")
+    if (unidad.length != 3) {
+        alert("Unidad de largo 3");
         return;
     }
 
-    materiasPrimas.push({
-        nombre: nombre,
-        cantidad: cantidad,
-        medida: medida
-    })
+    let materiaPrima = new MateriaPrima(nombre, unidad);
+
+    materiasPrimas.push(materiaPrima);
+
+    if (confirm("Definir precio?")) {
+        const importe = parseFloat(prompt("precio"))
+        if (isNaN(importe) || importe <= 0) {
+            alert("Importe negativo o cero invalido");
+            return;
+        }
+
+        const cantidad = parseFloat(prompt("cantidad"))
+        if (isNaN(cantidad) || cantidad <= 0) {
+            alert("Cantidad negativa o cero invalida");
+            return;
+        }
+
+        let precio = new Precio(importe, cantidad);
+
+        materiaPrima.definirPrecio(precio);
+    }
 }
 
 const mostrarMateriasPrimas = () => {
     for (const materiaPrima of materiasPrimas) {
-        console.log(materiaPrima);
+        console.log(materiaPrima.nombre, materiaPrima.importePorCantidad(1));
     }
 }
