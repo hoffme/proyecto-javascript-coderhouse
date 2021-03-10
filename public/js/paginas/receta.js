dbb89f19561cb993a93dbe593149b364a6dd97a7
Juan Coronel <phjocoronel@gmail.com>
2021-03-10 07:19:41 -0300

    creando slide pagina

--- public/js/paginas/receta.js
+++ public/js/paginas/receta.js
@@ -2,6 +2,15 @@ class PaginaRecetas extends Pagina {
     constructor() { super('Recetas') }
 
     _contenido() {
+        const boton = $('<button>Abrir Slide</button>');
+        boton.click(() => this.abrirSlide($('<h1>Holis</h1>')));
         
+        const boton2 = $('<button>Cerrar Slide</button>');
+        boton2.click(() => this.cerrarSlide());
+        
+        const ctn = $(`<div></div>`);
+        ctn.prepend(boton, boton2);
+
+        return ctn;
     }
 }
--- public/js/vistas/pagina.js
+++ public/js/vistas/pagina.js
@@ -1,7 +1,19 @@
 class Pagina {
     constructor(nombre) {
         this.nombre = nombre;
+        
         this.ctn = $(`<main class="pagina"></main>`);
+        this.slide = $(`<aside class="slide"></aside>`);
+    }
+
+    abrirSlide(contenido) {
+        this.slide.append(contenido);
+        this.slide.animate({ width: '100%' }, "fast");
+    }
+
+    cerrarSlide() {
+        this.slide.animate({ width: 0 }, "fast");
+        this.slide.empty();
     }
 
     _contenido() { }
@@ -10,6 +22,7 @@ class Pagina {
         this.ctn.empty();
         
         this.ctn.prepend(`<h1>${this.nombre}</h1>`);
+        this.ctn.prepend(this.slide);
         this.ctn.append(this._contenido());
         
         return this.ctn;
