223a45ce6eb91899ecd5a034637d7d620c2ac9da
Juan Coronel <phjocoronel@gmail.com>
2021-03-11 22:17:18 -0300

    crud herramientas creada

--- public/js/paginas/herramientas.js
+++ public/js/paginas/herramientas.js
@@ -1,5 +1,72 @@
-class PaginaHerramientas extends Pagina {
-    constructor() { super('Herramientas') }
+class PaginaHerramientas extends Crud {
+    constructor() { super('Herramientas', contexto.repositorios.herramientas) }
 
-    _contenido() {}
+    filtroBusqueda(filtro, obj) {
+        if (
+            Object.keys(filtro).length === 0 ||
+            filtro.query === ''
+        ) return true;
+        
+        if (filtro.query) {
+            if (obj.nombre && obj.nombre.toLowerCase().includes(filtro.query.toLowerCase())) return true;
+            if (obj.codigo && obj.codigo.toLowerCase().includes(filtro.query.toLowerCase())) return true;
+        }
+
+        return false;
+    }
+
+    filaCabecera() {
+        return $(`<div>
+            <label class="codigo">Codigo</label>
+            <label class="nombre">Nombre</label>
+            <label class="consumo">Consumo</label>
+            <label class="editar"></label>
+        </div>`);
+    }
+
+    fila(obj) {
+        const ctn = $(`<div class="listado-fila">
+            <label class="codigo">${obj.codigo}</label>
+            <label class="nombre">${obj.nombre}</label>
+            <label class="consumo">$ ${obj.consumo}</label>
+        </div>`);
+
+        const editar = $('<button>Editar</button>');
+        editar.click(() => this.editar(obj));
+        ctn.append(editar);
+
+        return ctn;
+    }
+
+    cabeceraBusqueda(contenedor) {
+        contenedor.append(
+            Texto({
+                placeholder: 'Buscar ...',
+                alCambiar: texto => this.buscar({ query: texto })
+            }),
+            BotonPrincipal({
+                titulo: 'Crear Herramienta',
+                alClick: () => this.crear()
+            })
+        );
+    }
+
+    formularioCreacion(datos) { return this.formulario(datos) }
+
+    formularioEdicion(datos) { return this.formulario(datos) }
+
+    formulario(datos) {
+        return [
+            Texto({ titulo: 'Codigo', valor: datos.codigo, alCambiar: t => datos.codigo = t }),
+            Texto({ titulo: 'Nombre', valor: datos.nombre, alCambiar: t => datos.nombre = t }),
+            Numero({ titulo: 'Consumo', placeholder: 'Watts/Hora', valor: datos.consumo, alCambiar: t => datos.consumo = t }),
+            Numero({ titulo: 'Capacidad', valor: datos.capacidad, alCambiar: t => datos.capacidad = t }),
+            Seleccion({ titulo: 'Unidad de Capacidad', valor: datos.unidad, alCambiar: t => datos.unidad = t, opciones: [
+                { titulo: 'Masa (gramos)', valor: 'masa' },
+                { titulo: 'Volumen (mililitros)', valor: 'volumen' },
+                { titulo: 'Latas (Medianas)', valor: 'lat-med' },
+                { titulo: 'Latas (Grandes)', valor: 'lat-gra' }
+            ]}),        
+        ]
+    }
 }
--- public/js/paginas/productos.js
+++ public/js/paginas/productos.js
@@ -16,24 +16,22 @@ class PaginaProductos extends Crud {
     }
 
     filaCabecera() {
-        const ctn = $(`<div class="listado-cabecera-producto"></div>`);
-
-        ctn.append(`<label class="codigo">Codigo</label>`);
-        ctn.append(`<label class="nombre">Nombre</label>`);
-        ctn.append(`<label class="precio">Precio</label>`);
-        ctn.append(`<label class="cantidad">Cantidad</label>`);
-        ctn.append(`<label class="editar"></label>`);
-
-        return ctn;
+        return $(`<div>
+            <label class="codigo">Codigo</label>
+            <label class="nombre">Nombre</label>
+            <label class="precio">Precio</label>
+            <label class="cantidad">Cantidad</label>
+            <label class="editar"></label>
+        </div>`);
     }
 
     fila(obj) {
-        const ctn = $(`<div class="listado-fila-producto"></div>`);
-
-        ctn.append(`<label class="codigo">${obj.codigo}</label>`);
-        ctn.append(`<label class="nombre">${obj.nombre}</label>`);
-        ctn.append(`<label class="precio">$ ${obj.precio}</label>`);
-        ctn.append(`<label class="cantidad">${obj.cantidad}</label>`);
+        const ctn = $(`<div class="listado-fila">
+            <label class="codigo">${obj.codigo}</label>
+            <label class="nombre">${obj.nombre}</label>
+            <label class="precio">$ ${obj.precio}</label>
+            <label class="cantidad">${obj.cantidad}</label>
+        </div>`);
 
         const editar = $('<button>Editar</button>');
         editar.click(() => this.editar(obj));
@@ -64,12 +62,12 @@ class PaginaProductos extends Crud {
             Texto({ titulo: 'Codigo', valor: datos.codigo, alCambiar: t => datos.codigo = t }),
             Texto({ titulo: 'Nombre', valor: datos.nombre, alCambiar: t => datos.nombre = t }),
             Numero({ titulo: 'Precio', valor: datos.precio, alCambiar: t => datos.precio = t }),
+            Numero({ titulo: 'Cantidad', valor: datos.cantidad, alCambiar: t => datos.cantidad = t }),
             Seleccion({ titulo: 'Unidad de Medida', valor: datos.unidad, alCambiar: t => datos.unidad = t, opciones: [
                 { titulo: 'Masa (gramos)', valor: 'masa' },
                 { titulo: 'Volumen (mililitros)', valor: 'volumen' },
                 { titulo: 'Unitario', valor: 'unitario' },
             ]}),
-            Numero({ titulo: 'Cantidad', valor: datos.cantidad, alCambiar: t => datos.cantidad = t })
         ]
     }
 }
